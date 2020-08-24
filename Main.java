1595dacc3565a873ac2a6dfa9ecda35b74ca59e6
MattManTech <68231558+MattManTech@users.noreply.github.com>
2020-08-24 22:05:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,16 +14,16 @@ public class Main {
 		File f = new File("Rail.txt");
 		PrintWriter pw = new PrintWriter(f);
 
-		System.out.println("Welcome to train site!\n");
-		String starting, destination, leaving, arrival;
-
-		byte choise, counter = 0;
+				System.out.println("Welcome to train site!\n");
+		String starting, destination, leaving, arrival, midStation, midStationTime;
+		String midStations[] = null;
+		byte choise, counter = 0, numOdMidStations ;
 		boolean b = true;
 
 		while (b) {
 			System.out.printf(
 					"To schedule trip (number %d) press --> 1\nTo sort all trips by departure time press --> 2\nTo quit press--> 9\n\n",
-					counter);
+					(counter+1));
 			choise = read.nextByte();
 
 			switch (choise) {
@@ -39,8 +39,20 @@ public class Main {
 				destination = read.nextLine();
 				System.out.println("What is the arrival time?");
 				arrival = read.nextLine();
+				System.out.println("How many midStations there is?");
+				numOdMidStations=read.nextByte();
+				midStations= new String[numOdMidStations];
+				read.nextLine();
+				for (int i=0; i<numOdMidStations; i++) {
+					System.out.println("Enter midStation name number " + (i+1) + ":");					
+					midStation=read.nextLine();
+					System.out.println("Enter midStation arrival time:");
+					midStationTime=read.nextLine();
+					midStations[i]= "MidStation " + (i+1) + ": " + midStation + " - " + midStationTime + ",";
+					
+				}
 				
-				Rail r = new Rail(starting, destination, leaving, arrival);
+				Rail r = new Rail(starting, destination, leaving, arrival, midStations );
 				r.save(pw);
 
 				break;
