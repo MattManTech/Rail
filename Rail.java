15be6c24cc6fa2647bdc91434a20b9bb40393fe5
MattManTech <68231558+MattManTech@users.noreply.github.com>
2020-08-25 14:24:55 +0300

    Update Rail.java

--- Rail.java
+++ Rail.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 
 public class Rail {
 
-	private String starting;     // תחנת יציאה
+	private String starting; // תחנת יציאה
 	private String destination; // תחנה הגעה
 	private String leaving; // שעת יציאה
 	private String arrival; // שעת הגעה
+	private int midStationsNum; // מספר תחנות ביניים
+	private String midStations[]; // תחנות ביניים
 
-		public Rail(String starting, String destination, String leaving, String arrival, String[] midStations ) {
+	public Rail(String starting, String destination, String leaving, String arrival, int midStationsNum,
+			String[] midStations) {
 		this.starting = starting;
 		this.destination = destination;
 		this.leaving = leaving;
 		this.arrival = arrival;
-		this.midStations=midStations;
+		this.midStationsNum = midStationsNum;
+		this.midStations = midStations;
 	}
 
 	public Rail(Scanner s) {
@@ -23,31 +27,50 @@ public class Rail {
 		leaving = s.nextLine();
 		destination = s.nextLine();
 		arrival = s.nextLine();
+		midStations = new String[s.nextInt()];
+		s.nextLine();
+		for (int i = 0; i < midStations.length; i++) {
+			midStations[i] = s.nextLine();
+		}
+
 	}
 
-	public void save(PrintWriter pw) { 
+	public int getMidStationsNum() {
+		return midStationsNum;
+	}
+
+	public String[] getMidStations() {
+		return midStations;
+	}
+
+	public void save(PrintWriter pw) {
 		pw.println(starting);
 		pw.println(leaving);
 		pw.println(destination);
 		pw.println(arrival);
-		for (int i=0; i<midStations.length; i++) {
+		pw.println(midStationsNum);
+		for (int i = 0; i < midStations.length; i++) {
 			pw.println(midStations[i]);
 		}
 	}
-	
-	public void finalSave(PrintWriter pw) { //ùîéøä ñåôéú
+
+	public void finalSave(PrintWriter pw) { // שמירה סופית
 		pw.print(starting + ",");
 		pw.print(leaving + ",");
+		for (int i = 0; i < midStations.length; i++) {
+			pw.print(midStations[i] + ",");
+		}
 		pw.print(destination + ",");
 		pw.print(arrival);
-		pw.println("");
+		pw.println();
 	}
 
 	public String getleaving() {
 		return leaving;
 	}
-	
+
 	public String toString() {
-		return starting + "," + leaving + "," + destination + "," + arrival;
+		return starting + ", " + leaving + ", " + destination + ", " + arrival + ",";
 	}
+
 }
