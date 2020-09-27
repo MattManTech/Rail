146d306d6b9cd0cc35249a72a816e027eb65c85d
MattManTech <68231558+MattManTech@users.noreply.github.com>
2020-09-27 14:23:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static byte counter;
+
 	public static void main(String[] args) throws FileNotFoundException {
 
 		Scanner read = new Scanner(System.in);
@@ -28,117 +29,116 @@ public class Main {
 			choise = read.nextByte();
 
 			try {
-			
-			switch (choise) {
 
-			case 1: {
-				counter++;
-				read.nextLine();
-				System.out.println("From where? ");
-				starting = read.nextLine();
-				System.out.println("What is the departure time?");
-				leaving = read.nextLine();
-				System.out.println("Destination?");
-				destination = read.nextLine();
-				System.out.println("What is the arrival time?");
-				arrival = read.nextLine();
-				System.out.println("How many midStations there is?");
-				numOdMidStations = read.nextByte();
-				midStationName = new String[numOdMidStations];
-				midStationTime = new String[numOdMidStations];
-				read.nextLine();
-				for (int i = 0; i < numOdMidStations; i++) {
-					System.out.println("Enter midStation name number " + (i + 1) + ":");
-					midStationName[i] = read.nextLine();
-					System.out.println("Enter midStation arrival time:");
-					midStationTime[i] = read.nextLine();
-					
-				}
+				switch (choise) {
 
-				Rail r = new Rail(starting, destination, leaving, arrival, numOdMidStations, midStationName, midStationTime);
-				r.save(pw);
+				case 1: {
+					counter++;
+					read.nextLine();
+					System.out.println("From where? ");
+					starting = read.nextLine();
+					System.out.println("What is the departure time?");
+					leaving = read.nextLine();
+					System.out.println("Destination?");
+					destination = read.nextLine();
+					System.out.println("What is the arrival time?");
+					arrival = read.nextLine();
+					System.out.println("How many midStations there is?");
+					numOdMidStations = read.nextByte();
+					midStationName = new String[numOdMidStations];
+					midStationTime = new String[numOdMidStations];
+					read.nextLine();
+					for (int i = 0; i < numOdMidStations; i++) {
+						System.out.println("Enter midStation name number " + (i + 1) + ":");
+						midStationName[i] = read.nextLine();
+						System.out.println("Enter midStation arrival time:");
+						midStationTime[i] = read.nextLine();
 
-				break;
-			}
-
-			case 2:
-				pw.close();
-				Triplist t = new Triplist("Rail.txt", counter);
-				t.sort("Rail.txt");
-				t.finalSave("Rail.txt");
-				System.out.println(t.toString() + "\n");
-
-				break;
-
-			case 3:
-				pw.close();
-				System.out.println("From where? ");
-				read.nextLine();
-				starting = read.nextLine();
-				System.out.println("Destination?");
-				destination = read.nextLine();
-				System.out.println("What is the departure time?");
-				leaving = read.nextLine();
-				Triplist tk = new Triplist("Rail.txt", counter);
-				String upTo3Leaving[] = new String[3];
-				String upTo3Arrive[] = new String[3];
-
-				for (int i = 0; i < tk.getTripNum(); i++) {
-					if (starting.equals(tk.getRails()[i].getStarting())) {
-						if (tk.timeCheck(leaving, tk.getRails()[i].getLeaving()) == true) {
-							upTo3Leaving[i] = tk.getRails()[i].getLeaving();
-						}
-
-					} else if (starting.equals(tk.getRails()[i].getDestination())) {
-						break;
 					}
 
-					for (int j = 0; j < tk.getRails()[i].getMidStationsNum(); j++) {
-						if (starting.equals(tk.getRails()[i].getMidStationsNames()[j])
-								&& upTo3Leaving[i] == null) {
-							if (tk.timeCheck(leaving,
-									tk.getRails()[i].getMidStationsTimes()[j]) == true) {
-								upTo3Leaving[i] = tk.getRails()[i].getMidStationsTimes()[j];
+					Rail r = new Rail(starting, destination, leaving, arrival, numOdMidStations, midStationName,
+							midStationTime);
+					r.save(pw);
+
+					break;
+				}
+
+				case 2:
+					pw.close();
+					Triplist t = new Triplist("Rail.txt", counter);
+					t.sort("Rail.txt");
+					t.finalSave("Rail.txt");
+					System.out.println(t.toString() + "\n");
+
+					break;
+
+				case 3:
+					pw.close();
+					System.out.println("From where? ");
+					read.nextLine();
+					starting = read.nextLine();
+					System.out.println("Destination?");
+					destination = read.nextLine();
+					System.out.println("What is the departure time?");
+					leaving = read.nextLine();
+					Triplist tk = new Triplist("Rail.txt", counter);
+					String upTo3Leaving[] = new String[3];
+					String upTo3Arrive[] = new String[3];
+
+					for (int i = 0; i < tk.getTripNum(); i++) {
+						if (starting.equals(tk.getRails()[i].getStarting())) {
+							if (tk.timeCheck(leaving, tk.getRails()[i].getLeaving()) == true) {
+								upTo3Leaving[i] = tk.getRails()[i].getLeaving();
 							}
 
-						}
-						if (destination.equals(tk.getRails()[i].getDestination()) && upTo3Leaving[i] != null) {
-							upTo3Arrive[i] = tk.getRails()[i].getArrival();
-
+						} else if (starting.equals(tk.getRails()[i].getDestination())) {
+							break;
 						}
 
-						else if (destination.equals(tk.getRails()[i].getMidStationsNames()[j])
-								&& upTo3Leaving[i] != null) {
-							upTo3Arrive[i] = tk.getRails()[i].getMidStationsTimes()[j];
+						for (int j = 0; j < tk.getRails()[i].getMidStationsNum(); j++) {
+							if (starting.equals(tk.getRails()[i].getMidStationsNames()[j]) && upTo3Leaving[i] == null) {
+								if (tk.timeCheck(leaving, tk.getRails()[i].getMidStationsTimes()[j]) == true) {
+									upTo3Leaving[i] = tk.getRails()[i].getMidStationsTimes()[j];
+								}
 
+							}
+							if (destination.equals(tk.getRails()[i].getDestination()) && upTo3Leaving[i] != null) {
+								upTo3Arrive[i] = tk.getRails()[i].getArrival();
+
+							}
+
+							else if (destination.equals(tk.getRails()[i].getMidStationsNames()[j])
+									&& upTo3Leaving[i] != null) {
+								upTo3Arrive[i] = tk.getRails()[i].getMidStationsTimes()[j];
+
+							}
 						}
 					}
-				}
-				int r=0;
-				for (int i=0; i<3; i++) {
-					if (upTo3Leaving[i]!=null && upTo3Arrive[i]!=null) { 
-						r++;
+					int r = 0;
+					for (int i = 0; i < 3; i++) {
+						if (upTo3Leaving[i] != null && upTo3Arrive[i] != null) {
+							r++;
+						}
 					}
-				}
-				System.out.println("You have " + r + " next trips on schedule:");
-				for (int i=0; i<3; i++) {
-					if (upTo3Leaving[i]!=null && upTo3Arrive[i]!=null) {
-					System.out.println("Leaving at " + upTo3Leaving[i] + " , arriving at " + upTo3Arrive[i] + "\n");
-				}
-				}
-				
-				
+					System.out.println("You have " + r + " next trips on schedule:");
+					for (int i = 0; i < 3; i++) {
+						if (upTo3Leaving[i] != null && upTo3Arrive[i] != null) {
+							System.out.println("Leaving from " + starting + "at " + upTo3Leaving[i] + " , arriving to"
+									+ destination + "at " + upTo3Arrive[i] + "\n");
+						}
+					}
 
-				break;
+					break;
 
-			case 9:
-				b = false;
-				System.out.println("Good Bye :)");
-				break;
-			}
-			}
-			catch (Exception e) {
-				System.out.println("Not available option-- " + e.getMessage() + ", try again\n");
+				case 9:
+					b = false;
+					System.out.println("Good Bye :)");
+					break;
+				}
+			} catch (NullPointerException e) {
+				System.out.println("Not available option -- (not exist) , try again\n");
+			} catch (Exception e) {
+				System.out.println("Not available option " + e.getMessage() + " , try again\n");
 			}
 		}
 		read.close();
