1e0d41c36efd3b5ef19bdc3b9ba2b9ff88e9741e
MattManTech <68231558+MattManTech@users.noreply.github.com>
2020-10-11 09:16:21 +0300

    Update rail.py

--- rail.py
+++ rail.py
@@ -24,7 +24,7 @@ def rail_schedule():
         time = request.args.get('time')
     else:
         time = "html"
-    return subprocess.check_output(["java", "-classpath", "/home/afeka/workspace-session8/Session8/bin", "main/Main",
+    return subprocess.check_output(["java", "-classpath", "/home/afeka/tsclient/matan/eclipse-workspace/Rail/src", "railForPycharm/Main",
                                     outformat,departure,arrive,time])
 
 
